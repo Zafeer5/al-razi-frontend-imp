00fb50a5a824db13fc5f8ae27758baa0f2dfbaef
Zafeer5 <zafeerseo@gmail.com>
2026-05-29 11:07:20 +0500

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,19 @@ import AdminLogin from "./pages/AdminLogin";
 import AdminPanel from "./pages/AdminPanel";
 import StudentsDatabase from "./pages/StudentsDatabase";
 import MarksDatabase from "./pages/MarksDatabase";
-
+import TeacherLogin from "./pages/TeacherLogin"; // 1. Naya Import add kiya gaya hai
 
 export default function App() {
   return (
     <Router>
       <Routes>
         {/* Absolute Error-Free App Routing Table Map */}
+        
+        {/* Teacher / Main Interface */}
+        <Route path="/teacher-login" element={<TeacherLogin />} /> {/* 2. Naya Route add kiya gaya hai */}
         <Route path="/" element={<Dashboard />} />
+        
+        {/* Admin Interface */}
         <Route path="/login" element={<AdminLogin />} />
         <Route path="/admin-panel" element={<AdminPanel />} />
         <Route path="/admin-panel/students-database" element={<StudentsDatabase />} />
